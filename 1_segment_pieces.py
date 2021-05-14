5a78deb130700dee580b628c167a2f3fa413a5f6
清欢 <qinghuan@qinghuandeMacBook-Pro.local>
2021-05-14 19:00:20 +0200

    Extract pieces

--- 1_segment_pieces.py
+++ 1_segment_pieces.py
@@ -1,5 +1,6 @@
 # This script should take an image of the unsolved puzzle from ./puzzle_generator
 # and output N smaller images, each containing one piece to folder ./segmented
+
 import copy
 import cv2 as cv
 import numpy as np
